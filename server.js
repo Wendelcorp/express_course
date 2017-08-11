b4323358635be565331dce0d39ca49ff4d81f8e2
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-08-11 16:32:36 -0400

    Setting up for CRUD development

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 var express = require('express')
 var app = express();
-var path = require('path');
-const pug = require('pug');
 var mustacheExpress = require('mustache-express');
 
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+// GET      - Read
+// POST     - Create
+// PUT      - Update
+// DELETE   - Delete
+
+var data = {
+      "Subject": "Name of people I know",
+      "names": [
+          {"name": "Bryce"},
+          {"name": "Taise"},
+          {"name": "Janet"},
+          {"name": "Dan"},
+          {"name": "Mike"},
+          {"name": "Linda"},
+      ]
+  };
 
 app.get('/', (req, res)  => {
-  res.render('index.mustache', {
-    name: "Bryce"
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('aboutme.pug', {
-    title: 'Hello Express & Pug'
-  });
+  res.render('index.mustache', data)
 });
 
 app.listen(3000, function() {
