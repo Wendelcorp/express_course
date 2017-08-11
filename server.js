463fde3886dd6efae2b25c80a985a518243e967b
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-08-11 15:17:42 -0400

    Trying out Mustache.js with Node

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var express = require('express')
 var app = express();
 var path = require('path');
 const pug = require('pug');
+var mustacheExpress = require('mustache-express');
 
-app.get('/', function(req, res) {
-  res.render('index.pug');
-});
-app.get('/about', function(req, res) {
-  res.render('aboutme.pug');
-});
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
 
+app.get('/', (req, res)  => {
+  res.render('index.mustache', {
+    name: "Bryce"
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('aboutme.pug', {
+    title: 'Hello Express & Pug'
+  });
+});
 
 app.listen(3000, function() {
   console.log("Listening on port 3000")
