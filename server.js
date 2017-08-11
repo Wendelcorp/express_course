ae3321a86553fef16685abd01ac26f6b6e1cc9dd
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-08-11 13:22:54 -0400

    Exploring templating with Pug

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express();
 var path = require('path');
+const pug = require('pug');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.render('index.pug');
+});
+app.get('/about', function(req, res) {
+  res.render('aboutme.pug');
 });
 
 
