56cf28cc1ce41dee01078c426086e46db08d963c
Bryce Wendelaar <brycewendelaar@InteraXons-MacBook-Pro.local>
2017-08-11 22:52:31 -0400

    Configured Post method for form

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 var express = require('express')
 var app = express();
 var mustacheExpress = require('mustache-express');
+var bodyParser = require('body-parser')
 
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 // GET      - Read
 // POST     - Create
 // PUT      - Update
 // DELETE   - Delete
 
-var data = {
-      "Subject": "Name of people I know",
-      "names": [
-          {"name": "Bryce"},
-          {"name": "Taise"},
-          {"name": "Janet"},
-          {"name": "Dan"},
-          {"name": "Mike"},
-          {"name": "Linda"},
-      ]
-  };
+var data = {};
+data.tvShows = [];
+data.tvShows.push('Rick and Morty')
 
 app.get('/', (req, res)  => {
   res.render('index.mustache', data)
+});
+app.post('/', (req, res)  => {
+  data.tvShows.push(req.body.name);
+  res.redirect('/');
 });
 
 app.listen(3000, function() {
